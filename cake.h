5e5da7827abb96852d0df7d64d3752ae5e64063a
magikz <abriftwr@gmail.com>
2026-07-16 23:00:44 -0500

    Added first iteration of shell API

--- cake.h
+++ cake.h
@@ -1,9 +1,11 @@
 #pragma once
 
 #include <stdarg.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <vadefs.h>
+#include <wchar.h>
 
 #ifdef _WIN32
 #define WIN32_LEAN_AND_MEAN
@@ -17,12 +19,13 @@ extern void configure_build();
 
 typedef int (*stage_work)(void);
 
-typedef int ck_handle;
+typedef int ck_stage_handle;
+typedef void* ck_handle;
 
 typedef struct ck_stage
 {
     stage_work work;
-    ck_handle* dependencies;
+    ck_stage_handle* dependencies;
     int depCount;
 } ck_stage;
 
@@ -32,7 +35,25 @@ typedef struct
     char* name;
 } ck_stage_name_pair;
 
-// ===================================== INTERNAL =====================================
+typedef struct
+{
+    unsigned int count;
+    char** files;
+    void* data;
+} ck_file_group;
+
+typedef struct
+{
+    char** lines;
+    int count;
+} ck_shell_output;
+
+typedef struct
+{
+    void* shell;
+    ck_shell_output std_out;
+    ck_shell_output std_err;
+} ck_shell_info;
 
 typedef struct
 {
@@ -42,14 +63,14 @@ typedef struct
 
 static ck_state _internal_ck_state = {};
 
-#define ck_make_stage(name, work, ...) _impl_ck_make_stage(name, work, __VA_ARGS__, 0)
-ck_handle _impl_ck_make_stage(char* stage_name, stage_work work, ...)
+#define ck_make_stage(name, work, ...) _internal_ck_make_stage(name, work, __VA_ARGS__, 0)
+ck_stage_handle _internal_ck_make_stage(char* stage_name, stage_work work, ...)
 {
     int argCount = 0;
     va_list args;
 
     va_start(args, work);
-    while (va_arg(args, const ck_handle) != 0)
+    while (va_arg(args, const ck_stage_handle) != 0)
     {
         argCount++;
     }
@@ -62,7 +83,7 @@ ck_handle _impl_ck_make_stage(char* stage_name, stage_work work, ...)
     ck_stage_name_pair* pair = &state->stageNamePair[state->pairCount - 1];
     pair->stage.work = work;
     pair->stage.depCount = argCount;
-    if (argCount > 0) { pair->stage.dependencies = malloc(sizeof(ck_handle) * argCount); }
+    if (argCount > 0) { pair->stage.dependencies = malloc(sizeof(ck_stage_handle) * argCount); }
 
     int nameLength = strlen(stage_name);
     pair->name = malloc((nameLength + 1) * sizeof(char));
@@ -71,7 +92,7 @@ ck_handle _impl_ck_make_stage(char* stage_name, stage_work work, ...)
     va_start(args, work);
     for (int i = 0; i < argCount; ++i)
     {
-        const ck_handle depHandle = va_arg(args, const ck_handle);
+        const ck_stage_handle depHandle = va_arg(args, const ck_stage_handle);
         pair->stage.dependencies[i] = depHandle;
     }
     va_end(args);
@@ -92,7 +113,6 @@ void _internal_ck_execute_stage(ck_stage* stage)
 
     stage->work();
 }
-
 
 int main(int argc, char** argv)
 {
@@ -120,7 +140,128 @@ int main(int argc, char** argv)
     return 0;
 }
 
-// ===================================== INTERNAL =====================================
+#define CHUNK_SIZE 4096
+void _internal_ck_shell_output(ck_shell_output* output, ck_handle handle)
+{
+    output->lines = malloc(sizeof(char*) * 10);
+    output->count = 0;
+
+    DWORD chunkSize = 4096;
+    DWORD bytesRead;
+    char readBuffer[chunkSize];
+
+    while(1)
+    {
+        DWORD result = ReadFile(handle, readBuffer, chunkSize, &bytesRead, NULL);
+        if (!result || bytesRead == 0)
+        {
+            // done reading
+            break;
+        }
+
+        DWORD lastLine = 0;
+        for (DWORD i = 0; i < bytesRead; ++i)
+        {
+            if (readBuffer[i] == '\n')
+            {
+                DWORD carriageReturnPadding = 0;
+                if (readBuffer[i-1] == '\r')
+                {
+                    carriageReturnPadding = 1;
+                }
+                output->lines[output->count] = malloc(i - lastLine + 1);
+                memcpy(output->lines[output->count], readBuffer + lastLine, i - lastLine + 1);
+                output->lines[output->count][i - lastLine - carriageReturnPadding] = '\0';
+                output->count++;
+                lastLine = i + 1;
+            }
+        }
+    }
+}
+
+#define ck_shell(command, ...) _internal_ck_shell(0, command, __VA_ARGS__, 0)
+#define ck_shell_out(std_out, command, ...) _internal_ck_shell(std_out, command, __VA_ARGS__, 0)
+void _internal_ck_shell(ck_shell_output* std_out, char* command, ...)
+{
+#ifdef WIN32
+    char commandBuffer[1024];
+
+    va_list args;
+    va_start(args, command);
+    vsnprintf(commandBuffer, sizeof(commandBuffer), command, args);
+    va_end(args);
+
+    HANDLE std_out_read = INVALID_HANDLE_VALUE;
+    HANDLE std_out_write = INVALID_HANDLE_VALUE;
+
+    SECURITY_ATTRIBUTES saAttr =
+    {
+        .nLength = sizeof(SECURITY_ATTRIBUTES),
+        .bInheritHandle = TRUE,
+        .lpSecurityDescriptor = NULL,
+    };
+
+    if (std_out != 0 && !CreatePipe(&std_out_read, &std_out_write, &saAttr, 0))
+    {
+        // handle errors.
+        return;
+    }
+
+    STARTUPINFO si =
+    {
+        .cb = sizeof(STARTUPINFO),
+        .dwFlags = STARTF_USESTDHANDLES,
+        .hStdOutput = std_out == 0 ? GetStdHandle(STD_OUTPUT_HANDLE) : std_out_write,
+        .hStdError = std_out == 0 ? GetStdHandle(STD_ERROR_HANDLE) : std_out_write,
+        .hStdInput = GetStdHandle(STD_INPUT_HANDLE),
+    };
+
+    PROCESS_INFORMATION pi = {0};
+
+    if (!CreateProcess
+    (
+        NULL,
+        commandBuffer,
+        NULL,
+        NULL,
+        TRUE,
+        CREATE_NO_WINDOW,
+        NULL,
+        NULL,
+        &si,
+        &pi
+    ))
+    {
+        // handle errors.
+        return;
+    }
+
+    WaitForSingleObject(pi.hProcess, INFINITE);
+
+    if (std_out != 0)
+    {
+        CloseHandle(std_out_write);
+        _internal_ck_shell_output(std_out, std_out_read);
+        CloseHandle(std_out_read);
+    }
+
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+#else
+#endif
+}
+
+void ck_shell_free_output(ck_shell_output* output)
+{
+#ifdef WIN32
+    for (int i = 0; i < output->count; ++i)
+    {
+        free(output->lines[i]);
+    }
+    free(output->lines);
+#else
+#endif
+}
 
 BOOL ck_mkdir(char* path)
 {
@@ -134,20 +275,9 @@ BOOL ck_mkdir(char* path)
 BOOL ck_rmdir(char* path)
 {
 #ifdef _WIN32
-    int pathLength = strlen(path);
-    char* doubleNullPath = malloc(pathLength + 2);
-    strncpy_s(doubleNullPath, pathLength + 1, path, pathLength + 1);
-    doubleNullPath[pathLength] = '\0';
-    doubleNullPath[pathLength + 1] = '\0';
-
-    SHFILEOPSTRUCTA fileOp = 
-    {
-        .wFunc = FO_DELETE,
-        .pFrom = doubleNullPath,
-        .fFlags = FOF_NO_UI | FOF_NOCONFIRMATION,
-    };
-
-    return !SHFileOperationA(&fileOp);
+    char command[1024];
+    sprintf(command, "cmd.exe /c rmdir /s /q \"%s\"", path);
+    return system(command) == 0;
 #else
 #endif
 }
@@ -160,7 +290,7 @@ BOOL ck_rmfile(char* path)
 #endif
 }
 
-void* ck_create_file(char* path)
+ck_handle ck_create_file(char* path)
 {
 #ifdef _WIN32
     HANDLE file = CreateFile
@@ -184,7 +314,37 @@ void* ck_create_file(char* path)
 #endif
 }
 
-BOOL ck_write_file(void* handle, char* data, unsigned long dataSize)
+ck_handle ck_open_file(char* path)
+{
+#ifdef _WIN32
+    HANDLE file = CreateFile
+    (
+        path,
+        GENERIC_WRITE,
+        0,
+        NULL,
+        OPEN_ALWAYS,
+        FILE_ATTRIBUTE_NORMAL,
+        NULL
+    );
+
+    if (file == INVALID_HANDLE_VALUE)
+    {
+        return 0;
+    }
+    
+    return file;
+#else
+#endif
+}
+
+BOOL ck_close_file(ck_handle handle)
+{
+    CloseHandle(handle);
+    return 1;
+}
+
+BOOL ck_write_file(ck_handle handle, char* data, unsigned long dataSize)
 {
 #ifdef _WIN32
     DWORD dataWritten = 0;
@@ -200,7 +360,7 @@ BOOL ck_write_file(void* handle, char* data, unsigned long dataSize)
 #endif
 }
 
-BOOL ck_append_file(void* handle, char* data, unsigned long dataSize)
+BOOL ck_append_file(ck_handle handle, char* data, unsigned long dataSize)
 {
 #ifdef _WIN32
     SetFilePointer(handle, 0, NULL, FILE_END);
@@ -216,4 +376,85 @@ BOOL ck_append_file(void* handle, char* data, unsigned long dataSize)
     return 0;
 #else
 #endif
+}
+
+ck_file_group ck_match_files(char* pattern)
+{
+    ck_file_group result = {};
+    WIN32_FIND_DATA findFileData;
+
+    // figure out length of the base path.
+    unsigned int basepathSize = 0;
+    for (int i = strlen(pattern); i >= 0; --i)
+    {
+        if (pattern[i] == '/')
+        {
+            basepathSize = i + 1;
+            break;
+        }
+    }
+
+    // Find the total count of files.
+    HANDLE hFind = FindFirstFileA(pattern, &findFileData);
+    if (hFind != INVALID_HANDLE_VALUE)
+    {
+        do
+        {
+            result.count++;
+        }
+        while (FindNextFile(hFind, &findFileData) != 0);
+    }
+    else
+    {
+        // handle errors
+        __builtin_trap();
+    }
+    FindClose(hFind);
+
+    // Find and store filenames
+    hFind = FindFirstFileA(pattern, &findFileData);
+    if (hFind != INVALID_HANDLE_VALUE)
+    {
+        char** filenames = malloc(sizeof(char*) * result.count);
+        result.count = 0;
+
+        do
+        {
+            unsigned int filenameSize = strlen(findFileData.cFileName);
+            unsigned long fullFilenameSize = filenameSize + basepathSize + 1;
+
+            char* fullFilename = malloc(fullFilenameSize);
+
+            memcpy(fullFilename, pattern, basepathSize);
+            memcpy(fullFilename + basepathSize, findFileData.cFileName, filenameSize);
+
+            // add null terminator
+            *(char*)(fullFilename + basepathSize + filenameSize + 1) = '\0';
+
+            filenames[result.count] = fullFilename;
+            result.count++;
+        }
+        while (FindNextFile(hFind, &findFileData) != 0);
+
+        result.files = filenames;
+        result.data = hFind;
+    }
+    else
+    {
+        // handle errors
+        __builtin_trap();
+    }
+    FindClose(hFind);
+
+    return result;
+}
+
+void ck_free_matched_files(ck_file_group* fileGroup)
+{
+    for (unsigned int i = 0; i < fileGroup->count; ++i)
+    {
+        free(fileGroup->files[i]);
+    }
+
+    free(fileGroup->files);
 }
