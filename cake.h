2f97ec347b14428dfb4e249d5594eadca19abba4
magikz <abriftwr@gmail.com>
2026-07-21 21:30:05 -0500

    More improvements to shell API. Added more logging. Added some filename util functions

--- cake.h
+++ cake.h
@@ -7,6 +7,10 @@
 #include <vadefs.h>
 #include <wchar.h>
 
+#define CAKE_VERSION_MAJOR 0
+#define CAKE_VERSION_MINOR 1
+#define CAKE_VERSION_PATCH  0
+
 #ifdef _WIN32
 #define WIN32_LEAN_AND_MEAN
 #include "Windows.h"
@@ -17,10 +21,33 @@
 
 extern void configure_build();
 
-typedef int (*stage_work)(void);
-
 typedef int ck_stage_handle;
 typedef void* ck_handle;
+
+typedef enum
+{
+    CK_BINARY_EXECUTABLE,
+    CK_BINARY_SHARED,
+    CK_BINARY_STATIC
+} ck_binary_type;
+
+typedef enum
+{
+    CK_LOG_LEVEL_FATAL,
+    CK_LOG_LEVEL_ERROR,
+    CK_LOG_LEVEL_WARNING,
+    CK_LOG_LEVEL_INFO,
+    CK_LOG_LEVEL_DEBUG,
+    CK_LOG_LEVEL_TRACE,
+} ck_log_level;
+
+typedef enum
+{
+    CK_STAGE_FAILURE,
+    CK_STAGE_SUCCESS,
+} ck_stage_result;
+
+typedef ck_stage_result (*stage_work)(void);
 
 typedef struct ck_stage
 {
@@ -29,13 +56,6 @@ typedef struct ck_stage
     ck_stage_handle* dependencies;
     int depCount;
 } ck_stage;
-
-typedef struct
-{
-    unsigned int count;
-    char** files;
-    void* data;
-} ck_file_group;
 
 typedef struct
 {
@@ -51,18 +71,9 @@ typedef struct
     ck_shell_output std_err;
 } ck_shell_info;
 
-typedef enum
-{
-    CK_LOG_LEVEL_FATAL,
-    CK_LOG_LEVEL_ERROR,
-    CK_LOG_LEVEL_WARNING,
-    CK_LOG_LEVEL_INFO,
-    CK_LOG_LEVEL_DEBUG,
-    CK_LOG_LEVEL_TRACE,
-} ck_log_level;
-
 typedef struct
 {
+    void* projectState;
     ck_stage* stages;
     int stageCount;
     ck_log_level logLevel;
@@ -76,10 +87,6 @@ static ck_state _internal_ck_state =
 };
 
 // ==================================== LOG API ========================================
-
-static const char* level_strings[] = {"[FATAL]", "[ERROR]", "[WARNING]", "[INFO]", "[DEBUG]", "[TRACE]" };
-static const char* level_color[] = {"\033[1;97m\033[101m", "\033[1;31m", "\033[1;33m", "\033[0;97m", "\033[0;92m", "\033[0;34m"};
-static const char* color_reset = "\033[0m";
 
 #define CK_FATAL(format, ...) _internal_ck_log(0, CK_LOG_LEVEL_FATAL, format, ##__VA_ARGS__)
 #define CK_ERROR(format, ...) _internal_ck_log(0, CK_LOG_LEVEL_ERROR, format, ##__VA_ARGS__)
@@ -100,16 +107,17 @@ void _internal_ck_log(int internal, ck_log_level level, char* format, ...)
     if (internal && level > _internal_ck_state.internalLogLevel) { return; }
     if (!internal && level > _internal_ck_state.logLevel) { return; }
 
+    char* level_strings[] = {"[FATAL]", "[ERROR]", "[WARNING]", "[INFO]", "[DEBUG]", "[TRACE]" };
+    char* level_color[] = {"\033[1;97m\033[101m", "\033[1;31m", "\033[1;33m", "\033[0;97m", "\033[0;92m", "\033[0;34m"};
+    char* color_reset = "\033[0m";
+
     va_list ptr;
-
     char out_message[32000] = "";
-
     va_start(ptr, format);
     vsnprintf(out_message, 32000, format, ptr);
     va_end(ptr);
 
     char formatted_message[32000] = "";
-
     if (internal)
     {
         sprintf(formatted_message, "%s[CAKE] %s%s\n", level_color[level], out_message, color_reset);
@@ -133,6 +141,8 @@ void ck_set_internal_log_level(ck_log_level level)
 }
 
 // ==================================== LOG API ========================================
+
+// =================================== STAGE API =======================================
 
 #define ck_make_stage(name, work, ...) _internal_ck_make_stage(name, work, __VA_ARGS__, 0)
 ck_stage_handle _internal_ck_make_stage(char* stage_name, stage_work work, ...)
@@ -173,49 +183,67 @@ ck_stage_handle _internal_ck_make_stage(char* stage_name, stage_work work, ...)
 
 void _internal_ck_execute_stage(ck_stage* stage, int indentLevel)
 {
+    char indentBuffer[1000];
+    for (int i = 0; i < indentLevel*4; i += 4)
+    {
+        indentBuffer[i+0] = ' ';
+        indentBuffer[i+1] = ' ';
+        indentBuffer[i+2] = ' ';
+        indentBuffer[i+3] = ' ';
+    }
+    indentBuffer[indentLevel*4] = '\0';
+
     ck_state* state = &_internal_ck_state;
 
     for (int i = 0; i < stage->depCount; ++i)
     {
+        _CK_DEBUG("%s- Executing dependency (%d/%d): \"%s\"", indentBuffer, i + 1, stage->depCount, state->stages[stage->dependencies[i] - 1].name);
         ck_stage* dependencyStage = &state->stages[stage->dependencies[i] - 1];
         _internal_ck_execute_stage(dependencyStage, indentLevel + 1);
     }
 
-    _CK_INFO("Executing Stage: %s", stage->name);
     stage->work();
+
+    _CK_DEBUG("Finished: %s", stage->name);
 }
 
-int main(int argc, char** argv)
-{
-    _CK_INFO("---------- Starting Cake ----------");
-
-    configure_build();
-
-    ck_state* state = &_internal_ck_state;
-
-    if (argc > 1)
-    {
-        for (int i = 0; i < state->stageCount; ++i)
-        {
-            ck_stage stage = state->stages[i];
-            if (strcmp(argv[1], stage.name) == 0)
-            {
-                _internal_ck_execute_stage(&stage, 0);
-                break;
-            }
-        }
-    }
-    else
-    {
-        _internal_ck_execute_stage(&_internal_ck_state.stages[_internal_ck_state.stageCount - 1], 0);
-    }
-
-    _CK_INFO("------------ Finished -------------\n");
-
-    return 0;
-}
+// =================================== STAGE API =======================================
 
 // ===================================== SHELL API =====================================
+
+void _internal_ck_shell_sanitize(char* commandLine)
+{
+// NOTE: This is hacky, needs work to correctly differentiate command switches from paths.
+#ifdef WIN32
+    int substrStart = 0;
+    int substrEnd = 0;
+
+    size_t commandLength = strlen(commandLine);
+    for (int i = 0; i < commandLength; ++i)
+    {
+        // if we encounter whitespace or we are at the end of the loop:
+        if (commandLine[i] == ' ' || i == commandLength - 1)
+        {
+            substrEnd = i;
+            if (commandLine[substrStart] == '.' ||
+                commandLine[substrStart] == '"' ||
+                commandLine[substrStart + 1] == ':')
+            {
+                // this is not a command switch, fix slashes.
+                for (int j = substrStart; j < substrEnd; ++j)
+                {
+                    if (commandLine[j] == '/')
+                    {
+                        commandLine[j] = '\\';
+                    }
+                }
+            }
+            substrStart = i + 1;
+        }
+    }
+#else
+#endif
+}
 
 void _internal_ck_shell_output(ck_shell_output* output, ck_handle handle)
 {
@@ -286,7 +314,9 @@ ck_shell_output _internal_ck_shell(char* command, ...)
     char formattedCommand[32000];
     sprintf(formattedCommand, "cmd.exe /c %s", inCommand);
 
-    _CK_TRACE("%s", formattedCommand);
+    _internal_ck_shell_sanitize(formattedCommand);
+
+    _CK_TRACE("ck_shell: %s", formattedCommand);
 
     HANDLE std_out_read = INVALID_HANDLE_VALUE;
     HANDLE std_out_write = INVALID_HANDLE_VALUE;
@@ -363,8 +393,6 @@ void ck_shell_free_output(ck_shell_output* output)
 #endif
 }
 
-// ===================================== SHELL API =====================================
-
 #define ck_make_path(format, ...) _internal_ck_make_path(format, ##__VA_ARGS__)
 char* _internal_ck_make_path(char* format, ...)
 {
@@ -389,4 +417,107 @@ char* _internal_ck_make_path(char* format, ...)
     return path;
 #else
 #endif
+}
+
+// ===================================== SHELL API =====================================
+
+char* ck_make_binary_name(char* name, ck_binary_type type)
+{
+#ifdef WIN32
+    size_t nameLength = strlen(name) + 5; // .{exe|dll|lib} + null terminator
+    char* result = malloc(nameLength);
+
+    switch (type)
+    {
+        case CK_BINARY_EXECUTABLE:
+        {
+            snprintf(result, nameLength, "%s.exe", name);
+            break;
+        }
+        case CK_BINARY_SHARED:
+        {
+            snprintf(result, nameLength, "%s.dll", name);
+            break;
+        }
+        case CK_BINARY_STATIC:
+        {
+            snprintf(result, nameLength, "%s.lib", name);
+            break;
+        }
+    }
+
+    return result;
+#else
+#endif
+}
+
+char* ck_filename_extension(char* path)
+{
+    size_t length = strlen(path);
+
+    for (int i = length - 1; i > 0; --i)
+    {
+        if (path[i] == '\\') { return path + i + 1; }
+    }
+
+    return path;
+}
+
+char* ck_filename(char* path)
+{
+    size_t length = strlen(path);
+
+    int end = 0;
+    int begin = 0;
+
+    for (int i = length - 1; i > 0; --i)
+    {
+        if (path[i] == '.')
+        {
+            end = i;
+        }
+        if (path[i] == '\\')
+        {
+            begin = i + 1;
+            break;
+        }
+    }
+
+    int size = end - begin + 1;
+    char* result = malloc(size);
+    memcpy(result, path + begin, size);
+    result[size - 1] = '\0';
+
+    return result;
+}
+
+int main(int argc, char** argv)
+{
+    printf("\033[0;97m---------- Cake v%d.%d.%d ----------\033[0m\n\n", CAKE_VERSION_MAJOR, CAKE_VERSION_MINOR, CAKE_VERSION_PATCH);
+
+    configure_build();
+
+    ck_state* state = &_internal_ck_state;
+
+    if (argc > 1)
+    {
+        for (int i = 0; i < state->stageCount; ++i)
+        {
+            ck_stage stage = state->stages[i];
+            if (strcmp(argv[1], stage.name) == 0)
+            {
+                _CK_DEBUG("Executing Stage: %s", stage.name);
+                _internal_ck_execute_stage(&stage, 0);
+                break;
+            }
+        }
+    }
+    else
+    {
+        ck_stage* stage = &_internal_ck_state.stages[_internal_ck_state.stageCount - 1];
+        _CK_DEBUG("Executing Stage: %s", stage->name);
+        _internal_ck_execute_stage(stage, 0);
+    }
+
+    return 0;
 }
